c224662cafe83e95e6b3cbc372b7192451f2a38b
Mihir Rege <mihirrege@gmail.com>
2014-10-29 01:27:47 +0530

    Use forked say.js repo

--- index.js
+++ index.js
@@ -11,48 +11,22 @@ if(fs.existsSync("./config.json")){
     config = require("./config.json");
 }
 
-function speak () {
-    if (!speaking) {
-	speakingQueue = [];
-	return;
-    }
-    if (speakingQueue.length === 0) {
-	setTimeout(speak, 200);
-	return;
-    }
-    else {
-	console.log("Speak: ", speakingQueue[0].text);
-        var voice;
-        if( config.voice){
-            voice = config.voice;
-        }
-        else{
-            if(os.platform() === 'linux'){
-                voice = "voice_default";
-            }
-            else if( os.platform() === 'darwin'){
-                voice = "Vicki";
-            }
-        }
-        say.speak(voice , speakingQueue[0].text);
-	setTimeout(speak, speakingQueue[0].time);
-	if (speakingQueue[0].cb)
-	    setTimeout(speakingQueue[0].cb, speakingQueue[0].time * 0.95);
-	speakingQueue.splice(0, 1);
-    }
-}
-
-speak();
-
 xal.on('xi.event.output.text', function(state, next) {
     //Getting the first one
     var text = state.get('xi.event.output.text')[0].value;
-    var numWords = text.split(/ |\.|,|:|\d/).length;
-    speakingQueue.push({
-	text: text,
-	time: (text.length / 4.0 * 300) + (numWords * 4000 / text.length)
-    });
-    next(state);
+    var voice;
+    if(config.voice){
+        voice = config.voice;
+    }
+    else{
+        if(os.platform() === 'linux'){
+            voice = "voice_default";
+        }
+        else if( os.platform() === 'darwin'){
+            voice = "Vicki";
+        }
+    }
+    say.speak(voice ,text);
 });
 
 xal.start({name: 'Speak'});
