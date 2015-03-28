e427ef3c72cb6906face18372dee26d736a1e5e8
Vikrant Varma <vikrant.varma94@gmail.com>
2015-03-28 23:18:50 +0530

    Add command to stop speaking

--- index.js
+++ index.js
@@ -7,30 +7,40 @@ var speakingQueue = [];
 var config = {};
 
 var speaker = {
-    speak: null
+    speak: null,
+    stop: function() {}
 };
 
 if (os.platform() === 'linux') {
     var say = require('say');
-    speaker.speak = function(text) { 
-        return say.speak('google', text);
+    speaker.speak = function(text) {
+        return say.speak('tts', text);
     };
+    speaker.stop = say.stop;
 } else if (os.platform() === 'darwin') {
-    var say = require('say');
+    say = require('say');
     speaker.speak = function(text) {
         return say.speak('Vicki', text);
-    }
+    };
+    speaker.stop = say.stop;
 } else if (os.platform() === 'win32') {
     var edge = require('./src/edge');
     speaker.speak = function(text) {
         return edge.speak(text);
-    }
+    };
 }
 
 xal.on('xi.event.output.text', function(state, next) {
     //Getting the first one
     var text = state.get('xi.event.output.text')[0].value;
     speaker.speak(text);
+});
+
+xal.on('xi.event.input.text', function(state, next) {
+    var text = state.get('xi.event.input.text')[0].value;
+    if (text.match(/stop speaking|shut up|silent|quiet/i)) {
+        speaker.stop();
+    }
 });
 
 xal.start({name: 'Speak'});
