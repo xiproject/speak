b351a029895cec0e0076014605c3fcca6a8fe303
Vikrant Varma <vikrant.varma94@gmail.com>
2015-03-29 21:37:41 +0530

    Bugfix: Call next(state)

--- index.js
+++ index.js
@@ -52,13 +52,15 @@ xal.on('xi.event.output.text', function(state, next) {
     //Getting the first one
     var text = state.get('xi.event.output.text')[0].value;
     speaker.speak(text);
+    next(state);
 });
 
 xal.on('xi.event.input.text', function(state, next) {
     var text = state.get('xi.event.input.text')[0].value;
-    if (text.match(/stop speaking|shut up|silent|quiet/i)) {
+    if (text.match(/stop speaking|shut up|be silent|be quiet/i)) {
         speaker.stop();
     }
+    next(state);
 });
 
 xal.start({
